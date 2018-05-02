ca32e409190ce0d28d0d92b6c45697d4764a241e
Ben Adrian Sarmiento <benadriansarmiento@gmail.com>
2018-05-02 08:46:34 +0800

    Add app page handler

--- src/App/Handler/AppPageHandler.php
+++ src/App/Handler/AppPageHandler.php
@@ -2,10 +2,27 @@
 
 namespace App\Handler;
 
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Zend\Diactoros\Response\HtmlResponse;
+use Zend\Expressive\Template;
+
 /**
  * @author Ben Sarmiento <me@bensarmiento.com>
  */
-final class LoginPageHandler
+final class AppPageHandler implements RequestHandlerInterface
 {
+    private $template;
 
+    public function __construct(
+        Template\TemplateRendererInterface $template
+    ) {
+        $this->template      = $template;
+    }
+
+    public function handle(ServerRequestInterface $request) : ResponseInterface
+    {
+        return new HtmlResponse($this->template->render('app::app-page', []));
+    }
 }
--- src/App/Handler/AppPageHandlerFactory.php
+++ src/App/Handler/AppPageHandlerFactory.php
@@ -2,10 +2,18 @@
 
 namespace App\Handler;
 
+use Psr\Container\ContainerInterface;
+use Zend\Expressive\Template\TemplateRendererInterface;
+
 /**
  * @author Ben Sarmiento <me@bensarmiento.com>
  */
 final class AppPageHandlerFactory
 {
+    public function __invoke(ContainerInterface $container) : AppPageHandler
+    {
+        $template = $container->get(TemplateRendererInterface::class);
 
+        return new AppPageHandler($template);
+    }
 }
